24faa5cddd113ba6800229a90d3ff690ecdc45e7
Jake Newall <jake.newall.94@googlemail.com>
2017-08-11 13:45:08 +0100

    Made category 1 printout blog posts

--- app/routes/category1.js
+++ app/routes/category1.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   //Takes a model with parameters
   model() {
     //returns a record from the store for category1 which is the blog name
-    var check = this.store.findAll('registration');
+    var check = this.store.findAll('blog-post');
 //    debugger;
     return check;
   }
