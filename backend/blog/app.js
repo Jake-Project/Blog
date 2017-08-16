e315c1b9ddc18f9b76260341cb179392677e28cf
Jake Newall <jake.newall.94@googlemail.com>
2017-08-16 10:59:48 +0100

    Ember links to firebase. Going to change this to express. This is a restore point

--- backend/blog/app.js
+++ backend/blog/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+var cars = ["Saab", "Volvo", "BMW"];
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    //res.send('Hello World!')
+    res.send(cars[1])
 })
 
 app.listen(3000, function () {
